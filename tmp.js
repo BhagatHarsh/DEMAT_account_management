5d94c057650f1e3608737db07332d1fa58cb990f
BhagatHarsh <93080554+BhagatHarsh@users.noreply.github.com>
2023-04-04 16:41:23 +0530

    db connected

--- tmp.js
+++ tmp.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const {Client} = require('pg');
 
 const client = new Client({
-    host: 'localhost',
-    user: 'bhaguu',
-    port: 5432,
-    password: '123',
-    database: 'stock'
-})
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+)
 
 client.connect()
 
