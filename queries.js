80244fd7f677fd769d0dc87a5263da0b6e2287d5
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-04-06 02:41:48 +0000

    Inserting user done

--- queries.js
+++ queries.js
@@ -1,15 +1,52 @@
 const { pool } = require("./dbConfig");
+const bcrypt = require('bcrypt');
 
 
-const getUsers = async () => {
-  try {
-    const result = await pool.query('SELECT * FROM users');
-    return result.rows;
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error retrieving users from database');
-  }
-}
+const getUsers = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const result = await pool.query('SELECT * FROM users');
+      resolve(result.rows);
+    } catch (err) {
+      console.error(err);
+      reject('Error retrieving users from database');
+    }
+  });
+};
+
+/*
+INSERT INTO users (pan_number, pincode, password, first_name, last_name) VALUES ('123456789', 382418, '123', 'Harsh', 'Bhagat')
+*/
+
+
+const registerUser = (data) => {
+  return new Promise((resolve, reject) => {
+    // hash the password
+    bcrypt.hash(data.password, 10, (err, hash) => {
+      if (err) {
+        reject(err);
+      } else {
+        // SQL query with parameterized placeholders
+        const query = 'INSERT INTO users (pan_number, pincode, password, first_name, last_name) VALUES ($1, $2, $3, $4, $5)';
+
+        // array of parameter values to be passed to the query
+        const values = [data.pan_number, data.pincode, hash, data.first_name, data.last_name];
+
+        // execute the query with the parameter values
+        pool.query(query, values, (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve('Data inserted successfully');
+          }
+        });
+      }
+    });
+  });
+};
+
+
 module.exports = {
-  getUsers,
-}
+      getUsers,
+      registerUser
+    }
--- server.js
+++ server.js
@@ -1,20 +1,45 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const port = 3000
 const db = require('./queries')
 const {pool} = require('./dbConfig')
+const { waitForDebugger } = require('inspector')
 
-app.use(express.json())
+// Middleware
+
+// Parses details from a form
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('view engine','ejs'); 
+app.engine('ejs', require('ejs').__express);
+
 
 app.get('/', (request, response) => {
-
-  response.json({ info: 'Node.js, Express, and Postgres API' })
+  response.render( __dirname + '/views/register.ejs')
 })
 
 app.get('/users', async (req, res) => {
-  const val = await db.getUsers()
-  res.send(val[0])
+  try {
+    const val = await db.getUsers();
+    res.send(val);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving users from database');
+  }
 });
+
+app.post('/users/register', async (req, res) => {
+  console.log(req.body);
+  try {
+    const confirmation = await db.registerUser(req.body);
+    res.send(confirmation);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error inserting user data');
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
